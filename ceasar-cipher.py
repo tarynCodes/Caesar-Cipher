60b287e36bb90c930ee5444b7916f73cd66ca707
taryncodes@github.com <taryn.bilsborough@gmail.com>
2024-01-22 17:15:55 +0000

    added art for UI, improved code by adding modulo so user can input any number of shifts

--- ceasar-cipher.py
+++ ceasar-cipher.py
@@ -1,10 +1,8 @@
+from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
             'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']  # doubled up to ensure we can encrypt all letters
-
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
@@ -15,9 +13,9 @@ def caesar(message, shift_amount, direction_answer):
     for letter in message:
         index = alphabet.index(letter)
         if direction_answer == "encode":
-            shifted_index = index + shift_amount
+            shifted_index = (index + shift_amount) % 26
         elif direction_answer == "decode":
-            shifted_index = index - shift_amount
+            shifted_index = (index - shift_amount) % 26
         final_text += alphabet[shifted_index]
     print(f"Your {direction}d message is {final_text}")
 
